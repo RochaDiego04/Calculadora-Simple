19b824ea1f53a9837aad68f608569152c3ad5178
Diego Rocha <drochaesc@gmail.com>
2023-09-02 01:13:27 -0600

    Refactoring and adding empty field comprobation

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.calculadora
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,63 +12,89 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun sumar (view: View){
-        val txtNumero1 : TextView = findViewById(R.id.activity_main_numero1)
-        val txtNumero2 : TextView = findViewById(R.id.activity_main_numero2)
-        val btnSumar : TextView = findViewById(R.id.activity_main_button_sumar)
-        val editResultado : TextView = findViewById(R.id.activity_main_resultado)
+    fun comprobarCampo1(): Boolean {
+        val num1: TextView = findViewById(R.id.activity_main_numero1)
 
-        btnSumar.setOnClickListener{
+        if (num1.text.toString().trim().isEmpty()) {
+            num1.error = getString(R.string.activity_main_error)
+            return false
+        }
+
+        return true
+    }
+
+    fun comprobarCampo2(): Boolean {
+        val num2: TextView = findViewById(R.id.activity_main_numero2)
+
+        if (num2.text.toString().trim().isEmpty()) {
+            num2.error = getString(R.string.activity_main_error)
+            return false
+        }
+
+        return true
+    }
+
+    fun sumar(view: View) {
+        val txtNumero1: TextView = findViewById(R.id.activity_main_numero1)
+        val txtNumero2: TextView = findViewById(R.id.activity_main_numero2)
+        val editResultado: TextView = findViewById(R.id.activity_main_resultado)
+
+        val campo1Valido = comprobarCampo1()
+        val campo2Valido = comprobarCampo2()
+
+        if (campo1Valido && campo2Valido) {
             val num1 = txtNumero1.text.toString().toIntOrNull() ?: 0
             val num2 = txtNumero2.text.toString().toIntOrNull() ?: 0
             val resultado = num1 + num2
             editResultado.text = "Resultado: $resultado"
-
         }
     }
 
     fun restar (view: View){
-        val txtNumero1 : TextView = findViewById(R.id.activity_main_numero1)
-        val txtNumero2 : TextView = findViewById(R.id.activity_main_numero2)
-        val btnRestar : TextView = findViewById(R.id.activity_main_button_restar)
-        val editResultado : TextView = findViewById(R.id.activity_main_resultado)
+        val txtNumero1: TextView = findViewById(R.id.activity_main_numero1)
+        val txtNumero2: TextView = findViewById(R.id.activity_main_numero2)
+        val editResultado: TextView = findViewById(R.id.activity_main_resultado)
 
-        btnRestar.setOnClickListener{
+        val campo1Valido = comprobarCampo1()
+        val campo2Valido = comprobarCampo2()
+
+        if (campo1Valido && campo2Valido) {
             val num1 = txtNumero1.text.toString().toIntOrNull() ?: 0
             val num2 = txtNumero2.text.toString().toIntOrNull() ?: 0
             val resultado = num1 - num2
             editResultado.text = "Resultado: $resultado"
-
         }
     }
 
     fun multiplicar (view: View){
-        val txtNumero1 : TextView = findViewById(R.id.activity_main_numero1)
-        val txtNumero2 : TextView = findViewById(R.id.activity_main_numero2)
-        val btnMultiplicar : TextView = findViewById(R.id.activity_main_button_multiplicar)
-        val editResultado : TextView = findViewById(R.id.activity_main_resultado)
+        val txtNumero1: TextView = findViewById(R.id.activity_main_numero1)
+        val txtNumero2: TextView = findViewById(R.id.activity_main_numero2)
+        val editResultado: TextView = findViewById(R.id.activity_main_resultado)
 
-        btnMultiplicar.setOnClickListener{
+        val campo1Valido = comprobarCampo1()
+        val campo2Valido = comprobarCampo2()
+
+        if (campo1Valido && campo2Valido) {
             val num1 = txtNumero1.text.toString().toIntOrNull() ?: 0
             val num2 = txtNumero2.text.toString().toIntOrNull() ?: 0
             val resultado = num1 * num2
             editResultado.text = "Resultado: $resultado"
-
         }
     }
 
     fun dividir (view: View){
-        val txtNumero1 : TextView = findViewById(R.id.activity_main_numero1)
-        val txtNumero2 : TextView = findViewById(R.id.activity_main_numero2)
-        val btnDividir : TextView = findViewById(R.id.activity_main_button_dividir)
-        val editResultado : TextView = findViewById(R.id.activity_main_resultado)
+        val txtNumero1: TextView = findViewById(R.id.activity_main_numero1)
+        val txtNumero2: TextView = findViewById(R.id.activity_main_numero2)
+        val editResultado: TextView = findViewById(R.id.activity_main_resultado)
 
-        btnDividir.setOnClickListener{
+        val campo1Valido = comprobarCampo1()
+        val campo2Valido = comprobarCampo2()
+
+        if (campo1Valido && campo2Valido) {
             val num1 = txtNumero1.text.toString().toIntOrNull() ?: 0
             val num2 = txtNumero2.text.toString().toIntOrNull() ?: 0
             val resultado = num1 / num2
             editResultado.text = "Resultado: $resultado"
-
         }
     }
 }
